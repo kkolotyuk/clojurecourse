54cd5d300726b85bc9be3163b5b6ec0b89e6cbf1
K.Kolotyuk <kk@7bits.com>
2014-04-13 21:42:49 +0700

    Add operation functional but it does not work

--- week3/dsl-no-hints/src/dsl/core.clj
+++ week3/dsl-no-hints/src/dsl/core.clj
@@ -1,10 +1,11 @@
 (ns dsl.core
-  (:use clojure.walk))
+  (:use clojure.walk)
+  (:import [java.util Calendar]))
 
-(def cal (java.util.Calendar/getInstance))
+(def cal (Calendar/getInstance))
 (def today (java.util.Date.))
-(def yesterday (do (.add cal java.util.Calendar/DATE -1) (.getTime cal)))
-(def tomorrow (do (.add cal java.util.Calendar/DATE 2) (.getTime cal)))
+(def yesterday (do (.add cal Calendar/DATE -1) (.getTime cal)))
+(def tomorrow (do (.add cal Calendar/DATE 2) (.getTime cal)))
 
 (comment
   (defn one [] 1)
@@ -23,13 +24,51 @@
                (< d3 d4))
         (println "DSL works correctly")))))
 
+(defn date? [d]
+  (instance? java.util.Date d))
+
+(def op-sym '#{+ -})
+
+(def metric-sym '#{day days week weeks month months year years
+                   hour hours minute minutes second seconds})
+
+(def metric-map {'day Calendar/DATE
+                 'days Calendar/DATE
+                 'week Calendar/WEEK_OF_YEAR
+                 'weeks Calendar/WEEK_OF_YEAR
+                 'month Calendar/MONTH
+                 'months Calendar/MONTH
+                 'year Calendar/YEAR
+                 'years Calendar/YEAR
+                 'hour Calendar/HOUR_OF_DAY
+                 'hours Calendar/HOUR_OF_DAY
+                 'minute Calendar/MINUTE
+                 'minutes Calendar/MINUTE
+                 'second Calendar/SECOND
+                 'seconds Calendar/SECOND})
+
+(defn op? [code]
+  (and (list? code)
+       (= (count code) 4)
+       (op-sym (second code))
+       (metric-sym (last code))))
+
+(defn op->int [op n]
+  (if (= op '+)
+    n
+    (- n)))
+
+(defn smrt-op [date op n metric]
+  (doto (Calendar/getInstance)
+        (.setTime date)
+        (.add (metric-map metric) (op->int op n))))
+
+(def comp-sym '#{< > = <= >=})
+
 (defn comp? [code]
   (and (list? code)
        (= (count code) 3)
-       ('#{< > = <= >=} (first code))))
-
-(defn date? [d]
-  (instance? java.util.Date d))
+       (comp-sym (first code))))
 
 (defn smrt-comparison [op x y]
   (if (and (date? x) (date? y))
@@ -39,6 +78,7 @@
 (defn- disp [x]
   (cond
     (comp? x) :comp
+    (op? x) :operation
     (coll? x) :collection
     :else :scalar))
 
@@ -51,7 +91,12 @@
             (replace-expr (rest code))))))
 
 (defmethod replace-expr :comp [code]
+  (println :comp)
   `(smrt-comparison ~(first code) ~(second code) ~(last code)))
+
+(defmethod replace-expr :operation [code]
+  (println :operation)
+  `(smrt-op ~(first code) ~(second code) ~(nth code 2) ~(last code)))
 
 (defmethod replace-expr :scalar [code] code)
 
@@ -61,8 +106,13 @@
   (let [replaced-code (map replace-expr code)]
     `(do ~@replaced-code)))
 
-(replace-expr '(< 1 2))
+;;(replace-expr '(> today yesterday))
+;;(disp '(today + 1 day))
+;;(replace-expr '(today + 1 day))
 
-(macroexpand-1 '(with-datetime (> today yesterday)))
+(defn replace-expr-1 [code]
+  `(smrt-op ~(first code) ~(second code) ~(nth code 2) ~(last code)))
+(replace-expr-1 '(today + 1 day))
 
-(with-datetime (> today yesterday))
+;;(macroexpand-1 '(with-datetime (today - 2 days)))
+;;(macroexpand-1 '(with-datetime (> today yesterday)))
